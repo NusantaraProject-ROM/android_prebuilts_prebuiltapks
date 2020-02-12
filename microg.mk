ce9d09938ffcc405f28440fff3848a4744b615f1
Vachounet <vachounet@live.fr>
2020-02-12 18:32:02 +0100

    add AuroraOSS packages
    Store + services

--- microg.mk
+++ microg.mk
@@ -1,6 +1,8 @@
 # Product priv-app
 PRODUCT_PACKAGES += \
     additional_repos \
+    AuroraServices \
+    AuroraStore \
     com.google.android.maps.xml \
     com.google.android.maps.jar \
     FakeStore \
