0f44af97f9c496fa20d7f60f55eb771d34e8e975
Vachounet <vachounet@live.fr>
2020-02-12 18:25:30 +0100

    add a root makefile

---
+++ microg.mk
@@ -0,0 +1,13 @@
+# Product priv-app
+PRODUCT_PACKAGES += \
+    additional_repos \
+    com.google.android.maps.xml \
+    com.google.android.maps.jar \
+    FakeStore \
+    FDroid \
+    FDroidPrivilegedExtension \
+    GmsCore \
+    GsfProxy \
+    MozillaNlpBackend \
+    OpenWeatherMapWeatherProvider
+
