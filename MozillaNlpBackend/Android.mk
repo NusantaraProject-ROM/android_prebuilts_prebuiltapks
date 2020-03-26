2ca2cb31b71a510e98726f0a2f4ca832c2e3172a
maxwen <max.weninger@gmail.com>
2020-03-26 17:58:25 +0100

    add ACCESS_BACKGROUND_LOCATION permission
    Change-Id: Ia82cc82381e3901cceb644ccb969e4bedd178f35

--- MozillaNlpBackend/Android.mk
+++ MozillaNlpBackend/Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := MozillaNlpBackend
-LOCAL_SRC_FILES := org.microg.nlp.backend.ichnaea_20033.apk
+LOCAL_SRC_FILES := org.microg.nlp.backend.ichnaea.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 include $(BUILD_PREBUILT)
